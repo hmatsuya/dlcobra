54c03661884efa852e4a617f5a2bd409bf2f1bfb
Hiroaki Matsuyama <hmatsuya@gmail.com>
2025-04-04 22:29:55 +0900

    feat(utils): enhance psv_to_hcpe conversion with validation checks
    Add multiple validation checks in the psv_to_hcpe.py script to ensure data integrity during the conversion from PSV to HCPE format. The update includes:
    - Verifying the correctness of SFEN strings before setting them on the board.
    - Checking if the board state is valid after setting SFEN.
    - Ensuring that the evaluation score and best move are integers.
    - Validating that the best move is legal according to the current board state.
    - Confirming that the game result is within the expected range (0: DRAW, 1: BLACK_WIN, 2: WHITE_WIN).
    
    The script now counts and reports the number of successfully processed positions, the total output, and the error rate. Invalid entries are reported and skipped without halting the conversion process.

--- dlshogi/utils/psv_to_hcpe.py
+++ dlshogi/utils/psv_to_hcpe.py
@@ -14,16 +14,42 @@ hcpes = np.zeros(len(psvs), dtype=HuffmanCodedPosAndEval)
 print(f'input num = {len(psvs)}')
 
 board = Board()
-for psv, hcpe in zip(psvs, hcpes):
-    board.set_psfen(psv['sfen'])
-    board.to_hcp(hcpe['hcp'])
-    hcpe['eval'] = psv['score']
-    hcpe['bestMove16'] = move16_from_psv(psv['move'])
-    game_result = psv['game_result']
-    # gameResult -> 0: DRAW, 1: BLACK_WIN, 2: WHITE_WIN
-    if game_result == 1:
-        hcpe['gameResult'] = board.turn + 1
-    elif game_result == -1:
-        hcpe['gameResult'] = 2 - board.turn
+num_positions = 0
+for index, (psv, hcpe) in enumerate(zip(psvs, hcpes)):
+    try:
+        if not board.set_psfen(psv['sfen']):
+            raise ValueError('failed to set sfen')
+        if not board.is_ok():
+            raise ValueError('board is not ok')
+        hcpe['eval'] = psv['score']
+        board.to_hcp(hcpe['hcp'])
+
+        if not isinstance(hcpe['eval'], int):
+            raise ValueError('eval is not int')
+        
+        hcpe['bestMove16'] = move16_from_psv(psv['move'])
+        if not isinstance(hcpe['bestMove16'], int):
+            raise ValueError('bestMove16 is not int')
+        move = board.move_from_move16(hcpe['bestMove16'])
+        if not board.is_legal(move):
+            raise ValueError('illegal move')
+
+        game_result = psv['game_result']
+        # gameResult -> 0: DRAW, 1: BLACK_WIN, 2: WHITE_WIN
+        if game_result == 1:
+            hcpe['gameResult'] = board.turn + 1
+        elif game_result == -1:
+            hcpe['gameResult'] = 2 - board.turn
+        if hcpe['gameResult'] not in [0, 1, 2]:
+            raise ValueError('gameResult is not 0, 1, 2')
+
+        num_positions += 1
+    except(Exception) as e:
+        print(index, e, psv)
+        # np.delete(hcpes, num_positions, axis=0)
+
+print(f'position num = {num_positions}')
+print(f'output num = {len(hcpes)}')
+print(f'error rate = {1 - num_positions / len(psvs)}')
 
 hcpes.tofile(args.hcpe)
