a0d65fc468ac3ccfa17a3df24639e585c4b1b297
Hiroaki Matsuyama <hmatsuya@gmail.com>
2025-04-08 22:38:58 +0900

    fix(setup): comment out version check due to AttributeError

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 pipenv run python setup.py clean --all
 rm -rf build dist dlshogi.egg-info
 pipenv run pip install . --force-reinstall
-python -c "import dlshogi; print(dlshogi.__version__)"
+# python -c "import dlshogi; print(dlshogi.__version__)" # AttributeError: module 'dlshogi' has no attribute '__version__'
