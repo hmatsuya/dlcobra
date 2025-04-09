4ade2efac2d0b7dc1f235a29ce1bf188a00d87bb
Hiroaki Matsuyama <hmatsuya@gmail.com>
2025-04-09 19:39:11 +0900

    feat(network): increase number of middle blocks in PolicyValueNetwork to improve model capacity

--- dlshogi/network/policy_value_network_cr.py
+++ dlshogi/network/policy_value_network_cr.py
@@ -88,7 +88,7 @@ class PolicyValueNetwork(torch.nn. Module):
         num_channels=192,
         args=None,
         dropout=0.05,
-        num_middle_blocks=10,
+        num_middle_blocks=30,
     ):
         super(PolicyValueNetwork, self).__init__()
 
