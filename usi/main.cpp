1632ba4f83066d5576ff3ae7f15663411e4322cb
Hiroaki Matsuyama <hmatsuya@gmail.com>
2026-02-27 17:34:51 +0900

    feat(uct-search): extend GPU support from 8 to 16 devices
    - Increase max_gpu constant from 8 to 16 in UctSearch.h
    - Add UCT_Threads9 through UCT_Threads16 USI options in usi.cpp
    - Add DNN_Model9 through DNN_Model16 USI options in usi.cpp
    - Add DNN_Batch_Size9 through DNN_Batch_Size16 USI options in usi.cpp
    - Update model_paths array initialization to include all 16 models in main.cpp
    - Update new_thread array initialization to include all 16 thread options in main.cpp
    - Update new_policy_value_batch_maxsize array to include all 16 batch size options in main.cpp
    - Enables multi-GPU inference with up to 16 devices for improved parallel processing

--- usi/main.cpp
+++ usi/main.cpp
@@ -285,7 +285,7 @@ void MySearcher::doUSICommandLoop(int argc, char* argv[]) {
 			if (!initialized) {
 				// 各種初期化
 				InitializeUctSearch(options["UCT_NodeLimit"]);
-				const std::string model_paths[max_gpu] = { options["DNN_Model"], options["DNN_Model2"], options["DNN_Model3"], options["DNN_Model4"], options["DNN_Model5"], options["DNN_Model6"], options["DNN_Model7"], options["DNN_Model8"] };
+				const std::string model_paths[max_gpu] = { options["DNN_Model"], options["DNN_Model2"], options["DNN_Model3"], options["DNN_Model4"], options["DNN_Model5"], options["DNN_Model6"], options["DNN_Model7"], options["DNN_Model8"], options["DNN_Model9"], options["DNN_Model10"], options["DNN_Model11"], options["DNN_Model12"], options["DNN_Model13"], options["DNN_Model14"], options["DNN_Model15"], options["DNN_Model16"] };
 				// モデルファイル存在チェック
 				{
 					bool is_err = false;
@@ -317,8 +317,8 @@ void MySearcher::doUSICommandLoop(int argc, char* argv[]) {
 						}
 					}
 				}
-				const int new_thread[max_gpu] = { options["UCT_Threads"], options["UCT_Threads2"], options["UCT_Threads3"], options["UCT_Threads4"], options["UCT_Threads5"], options["UCT_Threads6"], options["UCT_Threads7"], options["UCT_Threads8"] };
-				const int new_policy_value_batch_maxsize[max_gpu] = { options["DNN_Batch_Size"], options["DNN_Batch_Size2"], options["DNN_Batch_Size3"], options["DNN_Batch_Size4"], options["DNN_Batch_Size5"], options["DNN_Batch_Size6"], options["DNN_Batch_Size7"], options["DNN_Batch_Size8"] };
+				const int new_thread[max_gpu] = { options["UCT_Threads"], options["UCT_Threads2"], options["UCT_Threads3"], options["UCT_Threads4"], options["UCT_Threads5"], options["UCT_Threads6"], options["UCT_Threads7"], options["UCT_Threads8"], options["UCT_Threads9"], options["UCT_Threads10"], options["UCT_Threads11"], options["UCT_Threads12"], options["UCT_Threads13"], options["UCT_Threads14"], options["UCT_Threads15"], options["UCT_Threads16"] };
+				const int new_policy_value_batch_maxsize[max_gpu] = { options["DNN_Batch_Size"], options["DNN_Batch_Size2"], options["DNN_Batch_Size3"], options["DNN_Batch_Size4"], options["DNN_Batch_Size5"], options["DNN_Batch_Size6"], options["DNN_Batch_Size7"], options["DNN_Batch_Size8"], options["DNN_Batch_Size9"], options["DNN_Batch_Size10"], options["DNN_Batch_Size11"], options["DNN_Batch_Size12"], options["DNN_Batch_Size13"], options["DNN_Batch_Size14"], options["DNN_Batch_Size15"], options["DNN_Batch_Size16"] };
 				SetThread(new_thread, new_policy_value_batch_maxsize);
 
 				if (options["Mate_Root_Search"] > 0) {
